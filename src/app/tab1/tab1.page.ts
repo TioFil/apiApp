f0e1f1d5ef0c36f2eba357572830418e905cef22
TioFil <50384915+TioFil@users.noreply.github.com>
2020-03-16 20:27:43 -0300

    finalisacao da API de usuarios

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -19,12 +19,21 @@ export class Tab1Page {
   //Lista para os usuarios
   public listaUsuarios = [];
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService) {
+    this.buscarUsuarios(this.page);
+  }
 
 public buscarUsuarios(pagina:Number){
   this.userService.listarUsuarios(pagina).subscribe(dados=>{
-    
-  })
+    // Dados para paginação
+    this.page = dados['page'];
+    this.totalPaginas = dados['total_pages'];
+    this.usuariosPagina = dados['per_page'];
+    this.totalUsuario = dados['total'];
+
+    // Dados para listagem de usuarios
+    this.listaUsuarios = dados['data'];    
+  });
 }
 
 }
